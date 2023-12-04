5654c1284c5729d0212ecc6e783137ee23fb8b6c
bysnakejnr <barisberber.ca@gmail.com>
2023-12-04 17:37:06 -0500

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { FaBeer } from "react-icons/fa";
+import '../css/navigation.css';
+
+const Navigation = () => {
+    const Menus =[
+        {name:"Active Calls", dis:"translate-x-32"},
+        {name:"Archived Calls", dis:"translate-x-48"}
+    ]
+
+    const [active, setActive] = useState("");
+
+    return (
+    
+        <div className="bg-white max-h-[4.4rem] px-6 rounded-t-xl">
+            <ul className="flex relative">
+            {Menus.map((menu, i)=>(
+                <li key={i} className="w-16 align-middle">
+                    <a className="flex flex-col text-center pt-6 border">
+                        <span className="text-xl cursor-pointer margin-auto border m-auto">
+                            <FaBeer className="align-middle"/>
+                        </span>
+                        <span>{menu.name}</span>
+                    </a>
+                </li>
+            ))}
+            </ul>
+
+        </div>
+    );
+
+};
+
+export default Navigation;
